5b72b7ffc49ee22883ce31028c8bbeb1e48fd2aa
unknown <romulonilsonhorn@gmail.com>
2025-07-03 15:03:42 -0300

    arquivo zerado

--- deepscan/lib/main.dart
+++ deepscan/lib/main.dart
@@ -1,34 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Text("A");
   }
 }
