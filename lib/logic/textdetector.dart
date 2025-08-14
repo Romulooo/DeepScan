6b4371c83a8482c788946e7329564f552250c96c
Rômulo <139631832+Romulooo@users.noreply.github.com>
2025-08-14 10:33:08 -0300

    Update textdetector.dart

--- lib/logic/textdetector.dart
+++ lib/logic/textdetector.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 // Aqui vai a chave da API Sapling's API
-String key = "0MPZ3GUM2XLKAKQW04QCOJ22YLQ2L9OF";
+String key = "<chave>";
 
 Future<double> verificarTexto(texto) async {
   final url = Uri.parse('https://api.sapling.ai/api/v1/aidetect');
